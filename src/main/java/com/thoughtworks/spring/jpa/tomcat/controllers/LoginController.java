a36394e96f447bb109829c06a691c6c66201cf4e
Tong Shen <tshen@thoughtworks.com>
2015-01-23 15:28:15 +0800

    add errormessage and sign in to login page

--- src/main/java/com/thoughtworks/spring/jpa/tomcat/controllers/LoginController.java
+++ src/main/java/com/thoughtworks/spring/jpa/tomcat/controllers/LoginController.java
@@ -3,14 +3,20 @@ package com.thoughtworks.spring.jpa.tomcat.controllers;
 import com.thoughtworks.spring.jpa.tomcat.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
 
+    public static final String LOGIN = "login";
+    public static final String LOGOUT = "logout";
     @Autowired
     LoginService loginService;
 
@@ -21,8 +27,17 @@ public class LoginController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String login (@RequestParam(value = "username", required = true) String username,
-                         @RequestParam(value = "password", required = true) String password) {
-        loginService.validateUser(username, password);
-        return "";
+                         @RequestParam(value = "password", required = true) String password,
+                         HttpServletRequest httpServletRequest, Model model) {
+        HttpSession httpSession = httpServletRequest.getSession();
+        if (loginService.validateUser(username, password)) {
+            httpSession.setAttribute("loginStatus", LOGIN);
+            return "homePage";//TODO: add a homePage
+        }
+        else {
+            httpSession.setAttribute("loginStatus", LOGOUT);
+            model.addAttribute("error", "Your username or password is not correct");
+            return "login";
+        }
     }
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -9,11 +9,13 @@
 </head>
 <body>
 <div class="container">
-    <form class="form-signin">
+    <form class="form-signin" method="post">
         <h1 class="signin-heading">Please Login</h1>
         <input class="form-control" name="username" type="text" placeholder="Username"/>
         <input class="form-control" name="password" type="password" placeholder="Password"/>
+        <div class="form-control has-error">${error}</div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+        <a class="btn btn-lg btn-primary btn-block" href="register">Sign in</a>
     </form>
 </div>
 </body>
