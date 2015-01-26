f4de342b1e7e9cfa4ecd9a0cc34240c99502f5e6
Jia Wei <wjia@thoughtworks.com>
2015-01-26 09:59:20 +0800

    add user table and some test data for registration.

---
+++ src/main/resources/db/migration/V3__create_user_basic_table.sql
@@ -0,0 +1,13 @@
+create table User_Basic (
+    id serial primary key,
+    firstName varchar(20) not null,
+    lastName varchar(20) not null,
+    email varchar(80) not null,
+    password varchar(40) not null,
+    status varchar(20),
+    createDate timestamp not null,
+    modifyDate timestamp not null
+);
+
+create SEQUENCE user_id_generator START 10000;
+
