e8d5c203ec0cd188e9018da417f5f80a05d89f59
Tong Shen <tshen@thoughtworks.com>
2015-01-27 15:22:29 +0800

    add logincontroller test, add test dependency in pom

--- src/main/webapp/WEB-INF/views/confirmLogout.jsp
+++ src/main/webapp/WEB-INF/views/confirmLogout.jsp
@@ -2,17 +2,15 @@
   Created by IntelliJ IDEA.
   User: tshen
   Date: 1/27/15
-  Time: 10:05 AM
+  Time: 2:12 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title></title>
-    <script type="text/javascript" src="/assets/js/logoutHelpers.js"></script>
 </head>
 <body>
-<h1>It's home page.</h1>
-<a>Logout</a>
+
 </body>
 </html>
---
+++ src/test/java/controllerTests/LoginControllerTest.java
@@ -0,0 +1,67 @@
+package controllerTests;
+
+import com.thoughtworks.spring.jpa.tomcat.controllers.LoginController;
+import com.thoughtworks.spring.jpa.tomcat.services.LoginService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class LoginControllerTest {
+
+    @InjectMocks
+    private LoginController loginController;
+    @Mock
+    private LoginService loginService;
+
+    private String username;
+    private String password;
+    private HttpServletRequest httpServletRequest;
+    private HttpServletResponse httpServletResponse;
+    private Model model;
+    private javax.servlet.http.HttpSession session;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        username = "username";
+        password = "password";
+        httpServletRequest = mock(HttpServletRequest.class);
+        httpServletResponse = mock(HttpServletResponse.class);
+        session = mock(HttpSession.class);
+        model = mock(Model.class);
+    }
+
+    @Test
+    public void shouldReturnLoginWhenLoginInvalidate() throws Exception {
+        when(loginService.validateUser(username, password)).thenReturn(false);
+        when(httpServletRequest.getSession()).thenReturn(session);
+
+        String actual = loginController.login(username, password, httpServletRequest, httpServletResponse, model);
+        String expect = "login";
+
+        assertThat(actual, is(expect));
+    }
+
+    @Test
+    public void shouldReturnHomeWhenLoginValidate() throws Exception {
+        when(loginService.validateUser(username, password)).thenReturn(true);
+        when(httpServletRequest.getSession()).thenReturn(session);
+
+        String actual = loginController.login(username, password, httpServletRequest, httpServletResponse, model);
+        String expect = "home";
+
+        assertThat(actual, is(expect));
+    }
+}
