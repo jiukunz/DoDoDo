b1227036ec1bddde1146715e64d20d565004e4fe
Jia Wei <wjia@thoughtworks.com>
2015-02-04 17:35:36 +0800

    update db_migrate to dev profile

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean compile flyway:clean flyway:migrate -Dflyway.configFile=./src/main/webapp/WEB-INF/flyway.properties
+mvn clean compile flyway:clean flyway:migrate -Dflyway.configFile=./src/main/webapp/WEB-INF/flyway.properties -P dev
