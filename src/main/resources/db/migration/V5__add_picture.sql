93f016abf4130b7ecd96a828843b5cd307b995cb
wangdengwu <dengwu.wang@gmail.com>
2015-02-05 16:12:10 +0800

    change real data picture

--- src/main/resources/db/migration/V5__add_picture.sql
+++ src/main/resources/db/migration/V5__add_picture.sql
@@ -1,3 +1,3 @@
-insert into picture(id, user_id, caption, location, keyword, price, visibility,createDate, modifyDate) values ('cfqhscDiSN8eo_cq_1',2, 'jiukun','china','idea', 123,true, '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
-insert into picture(id, user_id, caption, location, keyword, price, visibility,createDate, modifyDate) values ('cfqhscDiSN8eo_cq_2',2, 'jiukun','china','idea', 27,true, '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
+insert into picture(id, user_id, caption, location, keyword, price, visibility,createDate, modifyDate) values ('aebc5914-cc1d-43ec-a031-51bf3721b517',2, 'jiukun','china','idea', 123,true, '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
+insert into picture(id, user_id, caption, location, keyword, price, visibility,createDate, modifyDate) values ('74364aab-a538-41ae-b61c-16baa1b60451',2, 'jiukun','china','idea', 27,true, '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
 
