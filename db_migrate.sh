f4a67b8befd9b87b3dd7c12bd6f40e0ad744d1aa
wangdengwu <dengwu.wang@gmail.com>
2015-02-06 09:16:46 +0800

    add heroku database config

--- db_migrate.sh
+++ db_migrate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean compile flyway:clean flyway:migrate -Dflyway.configFile=./src/main/webapp/WEB-INF/flyway.properties -P dev
+mvn clean compile flyway:clean flyway:migrate -Dflyway.configFile=./src/main/webapp/WEB-INF/flyway_dev.properties -P dev
