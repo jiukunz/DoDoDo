e36b842c87588e295be07c729cf1f529b633c208
NarshaXU <narsha_xu@yahoo.com>
2015-02-13 16:58:19 +0800

    remove sql files for purchased pictures

--- src/main/resources/db/migration/V8__create_purchased_pictures_table.sql
+++
@@ -1,7 +0,0 @@
-create table Purchased_Pictures{
-    id serial primary key unique,
-    userId integer not null,
-    picId varchar(40) not null,
-    createDate timestamp not null,
-    modifyDate timestamp not null
-};
--- src/main/resources/db/migration/V9__add_purchased_pictures.sql
+++
@@ -1,3 +0,0 @@
-insert into Purchased_Pictures(id, userId, picId, createDate, modifyDate) values (1, 1, 'aebc5914-cc1d-43ec-a031-51bf3721b517', '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
-insert into Purchased_Pictures(id, userId, picId, createDate, modifyDate) values (2, 1, '74364aab-a538-41ae-b61c-16baa1b60451', '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
-insert into Purchased_Pictures(id, userId, picId, createDate, modifyDate) values (3, 2, 'aebc5914-cc1d-43ec-a031-51bf3721b517', '2015-01-23 17:28:31.758', '2015-01-23 17:28:31.758');
